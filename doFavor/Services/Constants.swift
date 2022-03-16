39f05851579487e88bd8518c429e700f1a7a7783
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2022-03-16 07:26:16 +0700

    MVVM Structure & Endpoint

---
+++ doFavor/Services/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  doFavor
+//
+//  Created by Phakkharachate on 16/3/2565 BE.
+//
+
+import Foundation
+
+struct Constants {
+    static let BASE_URL = "https://swuhelp.herokuapp.com"
+}
